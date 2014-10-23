6f124d82fe7fc77490bb3f63eac4328347975c29
Daniel Yu <dcyu93@gmail.com>
2014-10-22 23:45:54 -0500

    added features page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,9 @@ class PagesController < ApplicationController
   def landing
   end
 
+  def features
+  end
+  
   def contact
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'pages#landing'
 
   get '/contact' => 'pages#contact', as: :contact
+  get '/features' => 'pages#features', as: :features
   post '/contact' => 'pages#send_email', as: :send_email
 
   get '/about' => 'pages#about', as: :about
