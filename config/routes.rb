1b6ace70db4520c83e9cc121f7a347c0363e0c9e
Stelios Constantinides <sconstantinides@gmail.com>
2014-10-28 11:52:53 -0500

    working on landing, adding careers

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get '/features' => 'pages#features', as: :features
   get '/mission' => 'pages#mission', as: :mission
+  get '/careers' => 'pages#careers', as: :careers
   get '/contact' => 'pages#contact', as: :contact
   post '/contact' => 'pages#send_email', as: :send_email
 
