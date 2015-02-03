3dbcc7b98ca3ca27d09cb37fa914670cfd0fae11
Stelios Constantinides <sconstantinides@gmail.com>
2015-02-03 11:13:47 -0600

    removed biz dev position and added developer position

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/features' => 'pages#features', as: :features
   get '/mission' => 'pages#mission', as: :mission
   get '/careers' => 'pages#careers', as: :careers
-  get '/business-development-job-description' => 'pages#business_development'
+  get '/full-stack-developer-job-description' => 'pages#full_stack_developer'
 
   get '/contact' => 'pages#contact', as: :contact
   post '/contact' => 'pages#send_email', as: :send_email
