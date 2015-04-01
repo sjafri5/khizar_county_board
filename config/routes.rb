f592831e1b9e0ab0cd7ccd62a15137a8a8b08ae3
Stelios Constantinides <sconstantinides@gmail.com>
2015-04-01 11:44:19 -0500

    remove job posting and reword contact

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   get '/contact', to: redirect('/')
   get '/careers', to: redirect('/')
   get '/business-development-job-description', to: redirect('/')
+  get '/Full-Stack-Developer', to: redirect('/')
 
 end
