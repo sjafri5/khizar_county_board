a5e49e9cd375ecdd9b28bdb45bc82597b8981547
Stelios Constantinides <sconstantinides@gmail.com>
2015-07-04 13:16:14 -0500

    testing different names

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   root 'pages#landing'
+  get '/test1', to: 'pages#landing_test_1'
+  get '/test2', to: 'pages#landing_test_2'
   post '/contact' => 'pages#send_email', as: :send_email
 
   # Redirect old pages
